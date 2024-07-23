438f071d9a9278b9f9556bc985580e8a9c1d717d
ArnavSingha <102987879+ArnavSingha@users.noreply.github.com>
2024-07-23 18:23:54 +0530

    main styles...

---
+++ theme/components/popover.ts
@@ -0,0 +1,11 @@
+import type { ComponentMultiStyle } from "@yamada-ui/react"
+
+export const Popover: ComponentMultiStyle = {
+  baseStyle: {
+    container: {},
+    closeButton: {},
+    header: {},
+    body: {},
+    footer: {},
+  },
+}
